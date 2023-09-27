17cc9cc36b567f49a3e24db3ec9fdd2d5e87e788
Daya Shankar <136248195+dayashankar19@users.noreply.github.com>
2023-09-27 20:20:56 +0530

    Add files via upload

---
+++ portfolio-master/README.md
@@ -0,0 +1,3 @@
+# portfolio
+gidy.ai
+https://drive.google.com/drive/folders/1l1ot9Uu762R_070C6g6rADm4nC7iEczM
---
+++ portfolio-master/app.js
@@ -0,0 +1,2 @@
+// Internal Javascript is used within HTML File
+//For more Javascript click on projects to check weather app , notes app with React.js in the github repository.
